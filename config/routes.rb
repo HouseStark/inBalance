79b402c4aab22f3c559eb1bef944367c28ea7639
HouseStark <jmullin@smu.edu>
2015-06-29 00:18:43 -0700

    adding Nav Bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'about/index'
+   # You can have the root of your site routed with "root"
+  root 'pages#home'
 
-  get 'home/index'
+  get 'about' => "pages#about"
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'home#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
